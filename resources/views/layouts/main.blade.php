4533773b99e9fe416da9968fdb14238f1019088f
Alek Marcondes <89283892+kallemback@users.noreply.github.com>
2023-06-30 21:32:02 -0300

    css

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,35 +21,34 @@
         <link rel="stylesheet" href="/css/style.css">
         <script src="/js/script.js"></script>
 
-        
-
     </head>
-        <body>
-            <header>
-                <nav class="navbar navbar-expand-lg navbar-light"></nav>
-                    <div class="collapse navbar=-collapse" id="navbar">
-                        <a href="/" class="navbar-brand">
-                            <img src="/img/hdcevents_logo.png" alt="HDV Events">
-                        </a>
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a href="/" class="nav link">Eventos</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="/" class="nav link">Criar Eventos</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="/" class="nav link">Entrar</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="/" class="nav link">Cadastrar</a>
-                            </li>
-                        </ul>
-                    </div>
-            </header>
-        @yield('content')
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="collapse navbar=-collapse" id="navbar">
+                    <a href="/" class="navbar-brand">
+                        <img src="/img/hdcevents_logo.svg" alt="HDC Events">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Criar Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Cadastrar</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    @yield('content')
 
-        </body>
+    </body>
         <footer>
             <p>MKINFO ELETRONIC &copy; 2023 </p></footer>
     </html>
